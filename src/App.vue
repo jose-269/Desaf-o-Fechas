cfa290c4e75c44d1f7e7cb2becc0a439f7b2bea4
unknown <jlmv84Qgmail.com>
2021-05-07 14:06:20 -0400

    Carga inicial

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,74 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Cuenta regresiva</h1>
+    <h3>
+      {{ tiempo }}
+    </h3>
+    <button
+      @click="cronos(btn.val)"
+      v-for="(btn, index) in dataTime"
+      :key="index"
+      class="buton mx-4 mt-2 btn btn-danger"
+    >
+      {{ btn.min }}
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+
+  data() {
+    return {
+      tiempo: "00:00",
+      intervalo: "",
+      dataTime: [
+        {
+          min: "3s",
+          val: "3",
+        },
+        {
+          min: "1m",
+          val: "60",
+        },
+        {
+          min: "5m",
+          val: "300",
+        },
+        {
+          min: "30m",
+          val: "1800",
+        },
+      ],
+    };
+  },
+  watch: {
+    tiempo(v) {
+      if (v === 0) this.parar();
+    },
+  },
+  methods: {
+    cronos(v) {
+      this.tiempo = v;
+
+      this.intervalo = setInterval(() => {
+        this.tiempo--;
+        // let minute = Math.floor((v / 60) % 60);
+        // minute = (minute < 10)? 0 + minute : minute;
+        // let seconds = v % 60;
+        // seconds = (seconds < 10)? 0 + seconds : seconds;
+        // console.log(minute);
+        // console.log(seconds);
+        // this.tiempo = minute + ":" + seconds;
+        // console.log(this.tiempo);
+      }, 1000);
+    },
+    parar() {
+      clearInterval(this.intervalo);
+      this.tiempo = "00:00";
+    },
   },
 };
 </script>
